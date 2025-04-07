b971855a426d3fea57a7d108286dddc4c737015e
NiSiWe <nino.siegrist@gmail.com>
2025-04-07 08:00:00 +0000

    Your commit message

--- lib/models/inventory_entry.dart
+++ lib/models/inventory_entry.dart
@@ -1,19 +1,102 @@
-class InventoryType {
+enum Team { SPAEHER, AMEISLI }
+
+enum Einheit {
+  STUECK,
+  KISTE,
+  KARTON,
+  PAAR,
+  SACK,
+  TISCH,
+  HOLZ,
+  GUSS,
+  ALU,
+  METALL,
+  SET,
+  TEXTIL,
+  BUND,
+  PLASTIK,
+  PAPIER,
+  GELTEN,
+  SCHUESSEL,
+  PACK,
+  TOPF,
+  FARBEN,
+}
+
+class Artikel {
+  final int id;
   final String artikel;
+  final String lager;
+  final int menge;
+  final Einheit einheit;
+  final String? groesse;
+  final String rubrik;
+  final List<InventoryEntry> entries;
 
-  InventoryType({required this.artikel});
+  Artikel({
+    required this.id,
+    required this.artikel,
+    required this.lager,
+    required this.menge,
+    required this.einheit,
+    this.groesse,
+    required this.rubrik,
+    this.entries = const [],
+  });
 
-  factory InventoryType.fromJson(Map<String, dynamic> json) {
-    return InventoryType(artikel: json['artikel'] as String);
+  factory Artikel.fromJson(Map<String, dynamic> json) {
+    return Artikel(
+      id: json['id'] as int,
+      artikel: json['artikel'] as String,
+      lager: json['lager'] as String,
+      menge: json['menge'] as int,
+      einheit: Einheit.values.firstWhere(
+        (e) => e.toString() == 'Einheit.${json['einheit']}',
+      ),
+      groesse: json['groesse'] as String?,
+      rubrik: json['rubrik'] as String,
+      entries:
+          (json['entries'] as List<dynamic>?)
+              ?.map((e) => InventoryEntry.fromJson(e as Map<String, dynamic>))
+              .toList() ??
+          [],
+    );
   }
 }
 
 class InventoryEntry {
-  final InventoryType type;
+  final int id;
+  final DateTime startedAt;
+  final DateTime? returnedAt;
+  final Team teamName;
+  final int typeId;
+  final int amountOfItem;
+  final Artikel type;
 
-  InventoryEntry({required this.type});
+  InventoryEntry({
+    required this.id,
+    required this.startedAt,
+    this.returnedAt,
+    required this.teamName,
+    required this.typeId,
+    required this.amountOfItem,
+    required this.type,
+  });
 
   factory InventoryEntry.fromJson(Map<String, dynamic> json) {
-    return InventoryEntry(type: InventoryType.fromJson(json['type']));
+    return InventoryEntry(
+      id: json['id'] as int,
+      startedAt: DateTime.parse(json['startedAt'] as String),
+      returnedAt:
+          json['returnedAt'] != null
+              ? DateTime.parse(json['returnedAt'] as String)
+              : null,
+      teamName: Team.values.firstWhere(
+        (t) => t.toString() == 'Team.${json['teamName']}',
+      ),
+      typeId: json['typeId'] as int,
+      amountOfItem: json['amountOfItem'] as int,
+      type: Artikel.fromJson(json['type'] as Map<String, dynamic>),
+    );
   }
 }
